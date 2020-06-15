b0f91ad7e32e43a236904e73a42f727c78d4288f
bittencourt <anderson.bitt3ncourt@gmail.com>
2020-06-15 19:41:29 -0300

    Added DateUtil

---
+++ app/src/main/java/com/bittencourt/unittesting/util/DateUtil.kt
@@ -0,0 +1,40 @@
+package com.bittencourt.unittesting.util
+
+import java.lang.IllegalArgumentException
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+object DateUtil {
+    private const val FULL_DATE_FORMAT = "MM-yyyy"
+    private const val WRITTEN_MONTH_DATE_FORMAT = "MMM"
+
+    val currentMonthYearTimeStamp: String
+        get(){
+            val dateFormat =
+                SimpleDateFormat(
+                    FULL_DATE_FORMAT,
+                    Locale.ENGLISH
+                )
+            return dateFormat.format(Date())
+        }
+
+
+    fun getWrittenMonthFromTimestamp(timestamp: String): String {
+        val dateFormat =
+            SimpleDateFormat(
+                FULL_DATE_FORMAT,
+                Locale.ENGLISH
+            )
+
+        val date = dateFormat.parse(timestamp) ?: IllegalArgumentException("Invalid timestamp")
+
+        val monthDateFormat =
+            SimpleDateFormat(
+                WRITTEN_MONTH_DATE_FORMAT,
+                Locale.ENGLISH
+            )
+
+        return monthDateFormat.format(date)
+    }
+}
